fc1730b19d61962b85667d443c0066f792383299
erickcarreon96 <77118297+erickcarreon96@users.noreply.github.com>
2021-01-13 09:58:57 -0600

    septima version

--- src/main/java/com/erick/erickcalculator/domain/service/CalculatorService.java
+++ src/main/java/com/erick/erickcalculator/domain/service/CalculatorService.java
@@ -7,21 +7,17 @@ public class CalculatorService {
 
     public int sum(int number1, int number2) {
         return number1 + number2;
-//        return Integer.toString(number1 + number2);
     }
 
     public int subtraction(int number1, int number2) {
         return number1 - number2;
-//        return Integer.toString(number1 - number2);
     }
 
     public int multiplication(int number1, int number2) {
         return number1 * number2;
-//        return Double.toString(number1 * number2);
     }
 
     public Double division(double number1, double number2) {
         return number1 / number2;
-//        return Double.toString(number1 / number2);
     }
 }//Fin de class CalculatorService
--- src/main/java/com/erick/erickcalculator/web/controller/CalculatorController.java
+++ src/main/java/com/erick/erickcalculator/web/controller/CalculatorController.java
@@ -21,27 +21,23 @@ public class CalculatorController {
     @ResponseBody
     public int sum(@RequestParam("number1") int number1, @RequestParam("number2") int number2) {
         return  calculatorService.sum(number1,number2);
-        //return calculatorService.sum(Integer.valueOf(number1), Integer.valueOf(number2));
     }
 
     @GetMapping("/subtraction")
     @ResponseBody
     public int subtraction(@RequestParam("number1") int number1, @RequestParam("number2") int number2) {
         return  calculatorService.subtraction(number1,number2);
-  //      return calculatorService.subtraction(Integer.valueOf(number1), Integer.valueOf(number2));
     }
 
     @GetMapping("/multiplication")
     @ResponseBody
     public int multiplication(@RequestParam("number1") int number1, @RequestParam("number2") int number2) {
         return  calculatorService.multiplication(number1,number2);
- //       return calculatorService.multiplication(Integer.valueOf(number1), Integer.valueOf(number2));
     }
 
     @GetMapping("/division")
     @ResponseBody
     public Double division(@RequestParam("number1") double number1, @RequestParam("number2") double number2) {
         return  calculatorService.division(number1,number2);
- //       return calculatorService.division(Integer.valueOf(number1), Integer.valueOf(number2));
     }
 }//Fin de class CalculatorController
